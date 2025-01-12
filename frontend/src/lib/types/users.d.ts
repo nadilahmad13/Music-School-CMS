83c3b2e206bb9ba4a33beb1859a67854100f5af7
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2025-01-12 23:50:20 +0100

    feat: user types

---
+++ frontend/src/lib/types/users.d.ts
@@ -0,0 +1,15 @@
+export interface Student {
+  students_id: {
+    id: string
+    first_name: string
+    last_name: string
+  }
+}
+
+export interface Teacher {
+  teachers_id: {
+    id: string
+    first_name: string
+    last_name: string
+  }
+}
